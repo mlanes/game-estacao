8686e0093a2ad9794b572cfdf30f147583541e8a
Mário Lanes <mario.lanesjr@gmail.com>
2019-09-23 00:39:30 -0300

    Colocar algumas condições em IF ternário e padronização

--- js/script.js
+++ js/script.js
@@ -7,20 +7,19 @@ function playerX(event) {
   let clickElement = event.srcElement.innerHTML;
 
   if(letter != ''){
-    if (clickElement == "X" || clickElement == "O") {
-      alert("Opa, este quadrado já foi escolhido!");
+    if (clickElement == 'X' || clickElement == 'O') {
+      alert('Opa, este quadrado já foi escolhido!');
     } else {
       target.innerHTML = letter;
-      letter === "X" ? target.className = 'letterX' : target.className = 'letterO'; 
-      if (letter == "X") {
-        letter = "O";
-      } else {
-        letter = "X";
-      }
-      document.querySelector('#player-type h3').innerHTML = `É a vez do <span>Jogador "${letter}"</span>`;
-      letter === "X" ? document.querySelector('#player-type h3 span').className = 'letterX' : document.querySelector('#player-type h3 span').className = 'letterO'; 
+      letter === 'X' ? target.className = 'letterX' : target.className = 'letterO'; 
+      
+      //Aqui serve para direcionar que será o próximo a jogar
+      letter = letter === 'X' ? 'O' : 'X';  
+
+      document.querySelector('#player-type h3').innerHTML = `É a vez do <span>Jogador '${letter}'</span>`;
+      letter === 'X' ? document.querySelector('#player-type h3 span').className = 'letterX' : document.querySelector('#player-type h3 span').className = 'letterO'; 
     }
-    verif(target.innerHTML);
+    verify(target.innerHTML);
   } else {
     alert('Selecione um jogador para começar o game :)');
   }
@@ -30,14 +29,14 @@ for (elementTable of elementsTable) {
 }
 
 function chooseTypePlayer(event) {
-  if (event.srcElement.innerText === 'Jogador "X"') {
-    letter = 'X';
-  } else {
-    letter = 'O';
-  }
-  console.log(letter);
+  letter = event.srcElement.innerText === 'Jogador "X"' ? 'X' : 'O';
   document.querySelector('#player-type h3').innerHTML = `O <span>${event.srcElement.innerText}</span> vai começar`;
-  letter === "X" ? document.querySelector('#player-type h3 span').className = 'letterX' : document.querySelector('#player-type h3 span').className = 'letterO'; 
+
+  if(letter === 'X'){
+    document.querySelector('#player-type h3 span').className = 'letterX';
+  } else {
+    document.querySelector('#player-type h3 span').className = 'letterO';
+  } 
   buttonTypePlayers[0].setAttribute('disabled', 'disabled');
   buttonTypePlayers[1].setAttribute('disabled', 'disabled'); 
 };
@@ -45,7 +44,7 @@ for (buttonTypePlayer of buttonTypePlayers) {
   buttonTypePlayer.onclick = chooseTypePlayer;
 }
 
-function verif(valueElement) {
+function verify(valueElement) {
   let item11 = document.getElementById('item11').innerHTML;
   let item12 = document.getElementById('item12').innerHTML;
   let item13 = document.getElementById('item13').innerHTML;
@@ -56,26 +55,30 @@ function verif(valueElement) {
   let item32 = document.getElementById('item32').innerHTML;
   let item33 = document.getElementById('item33').innerHTML;
 
-  if (((item11 != '') && (item12 != '') && (item13 != '') && (item11 == item12) && (item12 == item13))
-    || ((item11 != '') && (item22 != '') && (item33 != '') && (item11 == item22) && (item22 == item33))
-    || ((item11 != '') && (item21 != '') && (item31 != '') && (item11 == item21) && (item21 == item31))
-    || ((item21 != '') && (item22 != '') && (item23 != '') && (item21 == item22) && (item22 == item23))
-    || ((item31 != '') && (item32 != '') && (item33 != '') && (item31 == item32) && (item32 == item33))
-    || ((item12 != '') && (item22 != '') && (item32 != '') && (item12 == item22) && (item22 == item32))
-    || ((item13 != '') && (item23 != '') && (item33 != '') && (item13 == item23) && (item23 == item33))
-    || ((item31 != '') && (item22 != '') && (item13 != '') && (item31 == item22) && (item22 == item13))) {
+  if (
+      ((item11 != '') && (item12 != '') && (item13 != '') && (item11 == item12) && (item12 == item13))
+      || ((item11 != '') && (item22 != '') && (item33 != '') && (item11 == item22) && (item22 == item33))
+      || ((item11 != '') && (item21 != '') && (item31 != '') && (item11 == item21) && (item21 == item31))
+      || ((item21 != '') && (item22 != '') && (item23 != '') && (item21 == item22) && (item22 == item23))
+      || ((item31 != '') && (item32 != '') && (item33 != '') && (item31 == item32) && (item32 == item33))
+      || ((item12 != '') && (item22 != '') && (item32 != '') && (item12 == item22) && (item22 == item32))
+      || ((item13 != '') && (item23 != '') && (item33 != '') && (item13 == item23) && (item23 == item33))
+      || ((item31 != '') && (item22 != '') && (item13 != '') && (item31 == item22) && (item22 == item13))
+    ) {
     setTimeout(() => {
       alert( `O jogador ${valueElement} ganhou, parabéns campeão!`);
       newGame();
     }, 250);
-  } else if (((item11 != '') && (item12 != '') && (item13 != ''))
-    && ((item11 != '') && (item22 != '') && (item33 != ''))
-    && ((item11 != '') && (item21 != '') && (item31 != ''))
-    && ((item21 != '') && (item22 != '') && (item23 != ''))
-    && ((item31 != '') && (item32 != '') && (item33 != ''))
-    && ((item12 != '') && (item22 != '') && (item32 != ''))
-    && ((item13 != '') && (item23 != '') && (item33 != ''))
-    && ((item31 != '') && (item22 != '') && (item13 != ''))) {
+  } else if (
+      ((item11 != '') && (item12 != '') && (item13 != ''))
+      && ((item11 != '') && (item22 != '') && (item33 != ''))
+      && ((item11 != '') && (item21 != '') && (item31 != ''))
+      && ((item21 != '') && (item22 != '') && (item23 != ''))
+      && ((item31 != '') && (item32 != '') && (item33 != ''))
+      && ((item12 != '') && (item22 != '') && (item32 != ''))
+      && ((item13 != '') && (item23 != '') && (item33 != ''))
+      && ((item31 != '') && (item22 != '') && (item13 != ''))
+    ) {
     setTimeout(() => {
       alert('Deu Velha!');
       newGame();
@@ -90,8 +93,8 @@ function newGame() {
       document.getElementById(idElement).innerHTML = '';
     } 
   }
+  letter = '';
   buttonTypePlayers[0].removeAttribute('disabled');
   buttonTypePlayers[1].removeAttribute('disabled'); 
   document.querySelector('#player-type h3').innerHTML = '';
-  letter = "";
 }
